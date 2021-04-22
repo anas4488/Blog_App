353b6f5e9a818958ee6d1c5dae73d1ef3c88af9e
anas4488 <anassayed63@gmail.com>
2021-04-22 23:41:17 +0530

    Tenth commit

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
-from django import forms
 from django.contrib.auth.models import User
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +9,15 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+class UserUpdateForm(forms.modelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+class ProfileUpdateForm(forms.modelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,3 +8,8 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()    
+
+        
