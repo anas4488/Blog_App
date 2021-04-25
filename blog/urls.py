4135d8d7ea5c207677bcfef470e07a1c6db25f11
anas4488 <anassayed63@gmail.com>
2021-04-25 22:52:15 +0530

    twelfth commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from .views import (PostListView, 
+    PostDetailView, 
+    PostCreateView
+)
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<str:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
 
 ]
