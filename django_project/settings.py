260c4088d0927a40dbc98b73c368316a98a43a59
anas4488 <anassayed63@gmail.com>
2021-07-11 13:37:16 +0530

    change of plan

--- django_project/settings.py
+++ django_project/settings.py
@@ -120,7 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -137,3 +136,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login' 
+
+
+STATIC_URL = '/static/'
